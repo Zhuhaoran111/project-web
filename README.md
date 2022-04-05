acd09442123a4bd49b9db5abfcea756b23c49bd5
zhuhaoran <zhuhaoran@ambition.com>
2022-04-05 16:12:39 +0800

    导航栏

--- README.md
+++ README.md
@@ -19,3 +19,7 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+<!-- 笔记 -->
+<el-menu :unique-opened="true">  -------这是只保持一个子菜单展开。
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,6 +2,7 @@
 <template>
   <el-container class="container">
     <el-header class="header">
+      <!--首页标题导航栏 -->
       <el-row>
         <el-col :span="4"
           ><div class="grid-content bg-purple">
@@ -21,7 +22,87 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <!-- 只保持一个子菜单展开 -->
+        <el-menu :unique-opened="true">
+          <!-- 这里的index主要是控制打开，如果都一样，则打开一个全部都会展开 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="1-1">
+              <i class="el-icon-s-custom"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index="2-1">
+              <i class="el-icon-s-custom"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="2-2">
+              <i class="el-icon-s-custom"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index="3-1">
+              <i class="el-icon-s-shop"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-s-shop"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-s-shop"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index="4-1">
+              <i class="el-icon-s-grid"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="5-1">
+              <i class="el-icon-s-data"></i>
+              <span>数据报表</span>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
